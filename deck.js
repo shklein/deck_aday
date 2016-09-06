e02899c1546d3d8da39b4db34bd070b65c10a7e1
Samantha Klein <saradurga@gmail.com>
2016-09-06 14:51:06 -0500

    deck.js w/rough outline

---
+++ deck.js
@@ -0,0 +1,18 @@
+/*
+*database set-up:
+  -each card as object w/image, number, suit, individual id?
+  -each suit w/specific type of workout
+
+  linked relationally? removes need for comparison...SQL db
+  store image in directory w/reference(url) in db
+
+*button to get started ->
+  -function that randomly selects a card, compares suits & presents appropriate workout based on suit & number
+*button for next workout ->
+  -runs same function, but excludes previous card?
+
+*initial display: description & button to get started -- select number of moves (5, 10, 15, 20)
+*card display: image plus description of specific move & reps, next move button .. will end when selected # of moves is reached?
+*finished display: option to start over
+
+*/
